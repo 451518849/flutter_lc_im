937155c058483859374c540e566ee06eea56d432
小发工作室 <451518849@qq.com>
2019-10-13 20:50:48 +0800

    remove FileProvider in mainfest

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/wangxiaofa/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/wangxiaofa/Desktop/flutter_plugin/LLCIM/flutter_lc_im/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/wangxiaofa/Desktop/flutter/bin/cache/artifacts/engine/ios"
