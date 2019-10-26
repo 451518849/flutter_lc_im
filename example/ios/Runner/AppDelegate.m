1fb62592d5cf20dc6f44d25cfefdb044ca8c9efa
小发工作室 <451518849@qq.com>
2019-10-26 18:20:54 +0800

    重写文档

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -2,83 +2,71 @@
 #include "GeneratedPluginRegistrant.h"
 #import <FakeChatKit/LCChatKit.h>
 #import <UserNotifications/UserNotifications.h>
+#import "FlutterLcImPlugin.h"
 
-//
+static NSObject<FlutterBinaryMessenger>* messager = nil;
+
+@interface AppDelegate ()<UNUserNotificationCenterDelegate>
+
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
-    [GeneratedPluginRegistrant registerWithRegistry:self];
-
+        
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
     
-    [AVOSCloud setServerURLString:@"https://avoscloud.com" forServiceModule:AVServiceModuleAPI];
-    // 配置 SDK 推送
-    [AVOSCloud setServerURLString:@"https://avoscloud.com" forServiceModule:AVServiceModulePush];
-    // 配置 SDK 云引擎
-    [AVOSCloud setServerURLString:@"https://avoscloud.com" forServiceModule:AVServiceModuleEngine];
-    // 配置 SDK 即时通讯
-    [AVOSCloud setServerURLString:@"https://router-g0-push.avoscloud.com" forServiceModule:AVServiceModuleRTM];
+    [self registerForRemoteNotification];
     
-    
+    if(launchOptions != nil){
+        //这个方法用于收到推送后发给原生处理，用于收到推送后打开指定页面
+        [FlutterLcImPlugin sendNotification:launchOptions];
+    }
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
-- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
-    [AVOSCloud handleRemoteNotificationsWithDeviceToken:deviceToken];
-}
-
-- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
-    if (application.applicationState == UIApplicationStateActive) {
-    } else {
-
-        [[LCChatKit sharedInstance] didReceiveRemoteNotification:userInfo];
-    }
-}
-
-- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *,id> *)options {
-    return YES;
-}
 
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    // ios 13.0前的处理方式
+    // [AVOSCloud handleRemoteNotificationsWithDeviceToken:deviceToken];
+    
+    // ios 13.0之后的处理方式
+        AVInstallation *installation = [AVInstallation defaultInstallation];
+       [installation setDeviceTokenFromData:deviceToken teamId:@"xxxxxx"];
+       [installation saveInBackground];
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"收到推送:%@",userInfo);
+    
+//    [FlutterLcImPlugin sendNotification:userInfo];
+
+    if (application.applicationState == UIApplicationStateActive) {
+    } else {
+        
+        [[LCChatKit sharedInstance] didReceiveRemoteNotification:userInfo];
+    }
+}
 
 ///=============================================================================
 /// @name 添加处理APNs通知回调方法
 ///=============================================================================
 
-#pragma mark -
-#pragma mark - UNUserNotificationCenterDelegate Method
-
-///**
-// * Required for iOS 10+
-// * 在前台收到推送内容, 执行的方法
-// */
-//- (void)userNotificationCenter:(UNUserNotificationCenter *)center
-//       willPresentNotification:(UNNotification *)notification
-//         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
-//    NSDictionary *userInfo = notification.request.content.userInfo;
-//    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
-//        //TODO:处理远程推送内容
-//        NSLog(@"%@", userInfo);
-//    }
-//    // 需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
-//    completionHandler(UNNotificationPresentationOptionAlert);
-//}
-
-#pragma mark -
-#pragma mark - UIApplicationDelegate Method
-
 /*!
  * Required for iOS 7+
  */
-- (void)application:(UIApplication *)application
-didReceiveRemoteNotification:(NSDictionary *)userInfo
-fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
     //TODO:处理远程推送内容
-    NSLog(@"%@", userInfo);
+    NSLog(@"处理远程推送内容:%@", userInfo);
+    [FlutterLcImPlugin sendNotification:userInfo];
     // Must be called when finished
     completionHandler(UIBackgroundFetchResultNewData);
 }
@@ -94,6 +82,118 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
 - (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
     NSLog(@"%s\n[无法注册远程提醒, 错误信息]\nline:%@\n-----\n%@\n\n", __func__, @(__LINE__), error);
 }
+
+/**
+ * 初始化UNUserNotificationCenter
+ */
+- (void)registerForRemoteNotification {
+    // iOS 10 兼容
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"10.0")) {
+        
+#if XCODE_VERSION_GREATER_THAN_OR_EQUAL_TO_8
+        
+        // 使用 UNUserNotificationCenter 来管理通知
+        UNUserNotificationCenter *uncenter = [UNUserNotificationCenter currentNotificationCenter];
+        // 监听回调事件
+        [uncenter setDelegate:self];
+        //iOS 10 使用以下方法注册，才能得到授权
+        [uncenter requestAuthorizationWithOptions:(UNAuthorizationOptionAlert+UNAuthorizationOptionBadge+UNAuthorizationOptionSound)
+                                completionHandler:^(BOOL granted, NSError * _Nullable error)
+         {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 [[UIApplication sharedApplication] registerForRemoteNotifications];
+             });
+             //TODO:授权状态改变
+             NSLog(@"%@" , granted ? @"授权成功" : @"授权失败");
+         }];
+        // 获取当前的通知授权状态, UNNotificationSettings
+        [uncenter getNotificationSettingsWithCompletionHandler:^(UNNotificationSettings * _Nonnull settings) {
+            
+            NSLog(@"%s\nline:%@\n-----\n%@\n\n", __func__, @(__LINE__), settings);
+            /*
+             UNAuthorizationStatusNotDetermined : 没有做出选择
+             UNAuthorizationStatusDenied : 用户未授权
+             UNAuthorizationStatusAuthorized ：用户已授权
+             */
+            if (settings.authorizationStatus == UNAuthorizationStatusNotDetermined) {
+                NSLog(@"未选择");
+            } else if (settings.authorizationStatus == UNAuthorizationStatusDenied) {
+                NSLog(@"未授权");
+            } else if (settings.authorizationStatus == UNAuthorizationStatusAuthorized) {
+                NSLog(@"已授权");
+            }
+        }];
+        
+#endif
+        
+    }
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    else if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0")) {
+        UIUserNotificationType types = UIUserNotificationTypeAlert |
+        UIUserNotificationTypeBadge |
+        UIUserNotificationTypeSound;
+        UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+        
+        [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    } else {
+        UIRemoteNotificationType types = UIRemoteNotificationTypeBadge |
+        UIRemoteNotificationTypeAlert |
+        UIRemoteNotificationTypeSound;
+        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    }
+#pragma clang diagnostic pop
+}
+
+
+#pragma mark UNUserNotificationCenterDelegate
+#pragma mark - 添加处理 APNs 通知回调方法
+///=============================================================================
+/// @name 添加处理APNs通知回调方法
+///=============================================================================
+
+#pragma mark -
+#pragma mark - UNUserNotificationCenterDelegate Method
+
+#if XCODE_VERSION_GREATER_THAN_OR_EQUAL_TO_8
+
+/**
+ * Required for iOS 10+
+ * 在前台收到推送内容, 执行的方法
+ */
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+    NSDictionary *userInfo = notification.request.content.userInfo;
+    if([notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //TODO:处理远程推送内容
+        NSLog(@"前台收到推送内容:%@", userInfo);
+//        [FlutterLcImPlugin sendNotification:userInfo];
+
+    }
+    // 需要执行这个方法，选择是否提醒用户，有 Badge、Sound、Alert 三种类型可以选择设置
+    completionHandler(UNNotificationPresentationOptionAlert);
+}
+
+/**
+ * Required for iOS 10+
+ * 在后台和启动之前收到推送内容, 点击推送后执行的方法
+ */
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)(void))completionHandler
+{
+    NSDictionary *userInfo = response.notification.request.content.userInfo;
+    if([response.notification.request.trigger isKindOfClass:[UNPushNotificationTrigger class]]) {
+        //TODO:处理远程推送内容
+        NSLog(@"后台和启动之前:%@", userInfo);
+        [FlutterLcImPlugin sendNotification:userInfo];
+    }
+    completionHandler();
+}
+
+#endif
 
 
 @end
